6cc63c454adb74d34d72d10ca41c57d2b31e2967
sunjin12 <sunjin12333@gmail.com>
2026-03-29 16:42:36 +0900

    Title: 연속된 숫자 만들기 2, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260329/연속된 숫자 만들기 2/create-consecutive-numbers-2.py
@@ -0,0 +1,11 @@
+pos = list(map(int, input().split()))
+pos.sort()
+a, b, c = pos[0], pos[1], pos[2]
+
+if a + 1 == b and b + 1 == c:
+    print(0)
+elif a + 1 == b or b + 1 == c:
+    print(1)
+else:
+    print(2)
+
