7a4710ed2935081cfa22e42bc7de30fbe461e1d5
sunjin12 <sunjin12333@gmail.com>
2026-04-01 14:29:30 +0900

    Title: 원형 순열에서의 인원 제거, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260401/원형 순열에서의 인원 제거/josephus-permutations.py
@@ -0,0 +1,12 @@
+from collections import deque
+
+n, k = map(int, input().split())
+q = deque()
+
+for i in range(1, n+1):
+    q.append(i)
+
+while q:
+    for i in range(k-1):
+        q.append(q.popleft())
+    print(q.popleft(), end=' ')
