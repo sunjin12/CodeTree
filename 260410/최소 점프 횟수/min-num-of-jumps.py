8cba119bb408f0d06bd7e08888e18e60b612fabf
sunjin12 <sunjin12333@gmail.com>
2026-04-10 16:03:31 +0900

    Title: 최소 점프 횟수, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260410/최소 점프 횟수/min-num-of-jumps.py
+++ 260410/최소 점프 횟수/min-num-of-jumps.py
@@ -1,22 +1,22 @@
 n = int(input())
 num = [0] + list(map(int, input().split()))
-min_jumps = [-1] * (n+1)
+min_jumps = [-1] * (n + 1)
 
-# i번째에서 갈 수 있는 자리들 계산
 def jump(i, jumps):
-    min_jumps[i] = jumps
-    # i == n이면 최솟값 확인
+    # 끝 지점에 도달하면 탐색 종료 (이미 아래 for문에서 최소값 갱신을 마침)
     if i == n:
-        if min_jumps[n] == -1:
-            min_jumps[n] = jumps
-        else:
-            min_jumps[n] = min(min_jumps[n], jumps)
         return
 
-    for j in range(1, num[i]+1):
-        if min_jumps[i+j] == -1 or min_jumps[i+j] > jumps + 1:
-            jump(i+j, jumps+1)
-   
+    for j in range(1, num[i] + 1):
+        # 수정 1: 점프한 위치(i + j)가 n을 넘지 않도록 경계값 체크
+        if i + j <= n:
+            # 수정 2: 방문하지 않았거나, 기존보다 더 적은 점프로 도달 가능한 경우만 탐색
+            if min_jumps[i + j] == -1 or min_jumps[i + j] > jumps + 1:
+                min_jumps[i + j] = jumps + 1  # 큐/스택에 넣기 전에 방문 처리 및 최소값 갱신
+                jump(i + j, jumps + 1)
+
+# 시작 위치 초기화
+min_jumps[1] = 0
 jump(1, 0)
 
 # 최소 점프 출력
