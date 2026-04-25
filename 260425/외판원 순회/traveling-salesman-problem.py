d47d0254f5a39cece5993bd9cc20eaaa1f9f285a
sunjin12 <sunjin12333@gmail.com>
2026-04-25 21:54:35 +0900

    Title: 외판원 순회, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260425/외판원 순회/traveling-salesman-problem.py
+++ 260425/외판원 순회/traveling-salesman-problem.py
@@ -1,26 +1,38 @@
+import sys
+
+# 입력 속도 향상
+input = sys.stdin.readline
+
 n = int(input())
 A = [list(map(int, input().split())) for _ in range(n)]
 
-# visited 배열
+# 시작점(0번)은 이미 방문한 것으로 간주
 visited = [False] * n
-
 min_value = int(1e9)
 
-# traverse 함수: 입력 - (현재 노드, 지금까지 방문 노드 수, 지금까지 비용)
-def traverse(cur, nums, values):
+def traverse(cur, count, current_cost):
     global min_value
-    
-    if nums == n and cur == 0:
-        min_value = min(min_value, values)
+
+    # 1. 가지치기: 현재 비용이 이미 발견한 최솟값보다 크면 더 볼 필요 없음
+    if current_cost >= min_value:
+        return
+
+    # 2. 종료 조건: 모든 도시를 방문했을 때
+    if count == n:
+        # 마지막 도시에서 시작점(0)으로 돌아가는 길이 있는지 확인
+        if A[cur][0] != 0:
+            min_value = min(min_value, current_cost + A[cur][0])
         return
 
     for i in range(n):
-        if A[cur][i] != 0 and visited[i] is False:
+        # 방문하지 않았고, 길이 연결되어 있는 경우
+        if not visited[i] and A[cur][i] != 0:
             visited[i] = True
-            traverse(i, nums+1, values+A[cur][i])
+            traverse(i, count + 1, current_cost + A[cur][i])
             visited[i] = False
 
-# 최소 비용 출력
-traverse(0, 0, 0)
+# 0번 노드부터 시작 (방문 처리 필수)
+visited[0] = True
+traverse(0, 1, 0)
 
 print(min_value)
